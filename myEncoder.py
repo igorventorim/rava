ae5b4fe15e974b51c887a6619c833236abcff2c7
Igor Ventorim <igor.ventorim@ufes.br>
2017-10-31 12:29:42 -0200

    Testing struct pNota

--- myEncoder.py
+++ myEncoder.py
@@ -2,4 +2,4 @@ import json
 class MyEncoder(json.JSONEncoder):
     def default(self, o):
         # return {k.lstrip('_'): v for k, v in vars(o).items()}
-        return {k.lstrip('_'): v for k, v in vars(o).items()}
+        return { v for k, v in vars(o).items()}
