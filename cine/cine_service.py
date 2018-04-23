4ac4e4715d05cbe179db96d2dd77138dfed4d14f
Igor Ventorim <igor.ventorim@gmail.com>
2018-04-23 13:53:10 -0300

    Implements cine module

--- cine/cine_service.py
+++ cine/cine_service.py
@@ -35,16 +35,15 @@ class CineService:
                 weekday = 3 - weekday
             programacoes = Programacao.query.filter(
                 Programacao.data.between(datetime.datetime.now().date()+ datetime.timedelta(days=weekday), datetime.datetime.now().date()+ datetime.timedelta(days=weekday)+ datetime.timedelta(days=6))).order_by(Programacao.data.asc())
-
+        msg = ""
         for programacao in programacoes:
-            msg = ""
-            msg += msg + str(programacao.get_filme().get_titulo()) + "\n"
-            msg += msg + str(programacao.get_date())+"\n"
-            msg += msg + str(programacao.get_horario())+"\n"
-            msg += msg +"====================================\n"
-            data = answer_view_templates.text(user_id, msg)
-            MessengerService.sendMessage(message, data)
+            msg +=  str(programacao.get_filme().get_titulo()) + "\n"
+            msg +=  str(programacao.get_date())+"\n"
+            msg += str(programacao.get_horario())+"\n"
+            msg += "====================================\n"
 
+        data = answer_view_templates.text(user_id, msg)
+        MessengerService.sendMessage(message, data)
 
 
 
