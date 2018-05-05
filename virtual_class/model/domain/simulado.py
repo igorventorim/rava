ee9392e13f27a942167fcbacae05df03abfc02bd
Igor Ventorim <igor.ventorim@geocontrol.com.br>
2018-05-05 16:48:07 -0300

    Começando a brincadeira

--- virtual_class/model/domain/simulado.py
+++ virtual_class/model/domain/simulado.py
@@ -3,7 +3,7 @@ from config.configuration import Configuration
 
 class Simulado(Configuration.db.Model):
 
-    __tablename__ = "question"
+    __tablename__ = "simulado"
     id = Configuration.db.Column(Configuration.db.BIGINT, autoincrement=True, primary_key=True)
     questao = Configuration.db.Column(Configuration.db.String(50), unique=True, nullable=False)
     resposta = Configuration.db.Column(Configuration.db.String(1000), nullable=False)
