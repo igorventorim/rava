428c9f6fb5a1a2b5a09a5f598b1aaa45f4a5366d
Igor Ventorim <igor.ventorim@geocontrol.com.br>
2018-06-10 17:51:04 -0300

    Corrigindo multiplas mensagens

--- cine/cine_service.py
+++ cine/cine_service.py
@@ -13,33 +13,33 @@ class CineService:
 
     def getProgramacao(self,message):
         user_id = message.getClientID()
-        if 'datetime' in message.getEntities():
-            datenow = datetime.datetime.strptime(message.getEntities()['datetime'][0]['value'][:10], "%Y-%m-%d")
-            type = message.getEntities()['datetime'][0]['grain']
-            if type == "day":
-                programacoes = Programacao.query.filter_by(data=datenow.date()).order_by(Programacao.data.asc())
-            elif type == "week":
-                programacoes = Programacao.query.filter(
-                    Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=7))).order_by(Programacao.data.asc())
-            elif type == "month":
-                programacoes = Programacao.query.filter(
-                    Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=30))).order_by(Programacao.data.asc())
-            elif type == "year":
-                programacoes = Programacao.query.filter(
-                    Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=365))).order_by(Programacao.data.asc())
-        else:
-            weekday = datetime.datetime.today().weekday()
-            if weekday < 3:
-                weekday = -4 - weekday
-            else:
-                weekday = 3 - weekday
-            programacoes = Programacao.query.filter(
-                Programacao.data.between(datetime.datetime.now().date()+ datetime.timedelta(days=weekday), datetime.datetime.now().date()+ datetime.timedelta(days=weekday)+ datetime.timedelta(days=6))).order_by(Programacao.data.asc())
-        msg = ""
-        for programacao in programacoes:
-            msg += str(programacao.get_filme().get_titulo()) + "\n"
-            msg += str(programacao.get_date())+" - " + str(programacao.get_horario())+"\n\n"
-
+        # if 'datetime' in message.getEntities():
+        #     datenow = datetime.datetime.strptime(message.getEntities()['datetime'][0]['value'][:10], "%Y-%m-%d")
+        #     type = message.getEntities()['datetime'][0]['grain']
+        #     if type == "day":
+        #         programacoes = Programacao.query.filter_by(data=datenow.date()).order_by(Programacao.data.asc())
+        #     elif type == "week":
+        #         programacoes = Programacao.query.filter(
+        #             Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=7))).order_by(Programacao.data.asc())
+        #     elif type == "month":
+        #         programacoes = Programacao.query.filter(
+        #             Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=30))).order_by(Programacao.data.asc())
+        #     elif type == "year":
+        #         programacoes = Programacao.query.filter(
+        #             Programacao.data.between(datenow.date(), datenow.date() + datetime.timedelta(days=365))).order_by(Programacao.data.asc())
+        # else:
+        #     weekday = datetime.datetime.today().weekday()
+        #     if weekday < 3:
+        #         weekday = -4 - weekday
+        #     else:
+        #         weekday = 3 - weekday
+        #     programacoes = Programacao.query.filter(
+        #         Programacao.data.between(datetime.datetime.now().date()+ datetime.timedelta(days=weekday), datetime.datetime.now().date()+ datetime.timedelta(days=weekday)+ datetime.timedelta(days=6))).order_by(Programacao.data.asc())
+        # msg = ""
+        # for programacao in programacoes:
+        #     msg += str(programacao.get_filme().get_titulo()) + "\n"
+        #     msg += str(programacao.get_date())+" - " + str(programacao.get_horario())+"\n\n"
+        msg = "Devido a alguns problemas com o site do cine metrópolis temporiamente não estou sabendo dos filmes! ;)"
         data = answer_view_templates.text(user_id, msg)
         MessengerService.sendMessage(message, data)
 
--- messenger/messenger_service.py
+++ messenger/messenger_service.py
@@ -52,15 +52,13 @@ class MessengerService:
                             Configuration.db.session.commit()
                         except:
                             print("Erro ao cadastrar o usuário: "+message.getClientID())
-
+                    MessengerService.sendMessage(None, answer_view_templates.mark_seen(message.getClientID()))
+                    MessengerService.sendMessage(None, answer_view_templates.typing_on(message.getClientID()))
                     if not self.redis.existsUserOn(message.getClientID()+"_msg"):
                         self.redis.setKey(str(message.getClientID())+"_msg","Ativo")
                         self.redis.setExpire(message.getClientID()+"_msg",60)
                     else:
                         return
-
-                    MessengerService.sendMessage(None, answer_view_templates.mark_seen(message.getClientID()))
-                    MessengerService.sendMessage(None, answer_view_templates.typing_on(message.getClientID()))
                     self.__selector(message)
 
 
